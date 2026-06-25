dd25fab1967b7741e1f33fcaee5110b1781fb748
Sumarmahadev <sumarchimacode@gmail.com>
2026-06-25 06:10:58 +0530

    Delete backend/backend directory

--- backend/backend/User/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from . import views
-
-urlpatterns = [
-    path('register/', views.RegisterView.as_view(),          name='register'),
-    path('login/',    views.EmailTokenObtainPairView.as_view(), name='login'),
-    path('refresh/',  TokenRefreshView.as_view(),             name='token-refresh'),
-]
